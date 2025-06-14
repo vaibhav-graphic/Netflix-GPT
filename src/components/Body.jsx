cfb93ae72d74babc8d59576b67f2829ba5cdb79d
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-06-14 12:06:36 +0530

    add Routing

---
+++ src/components/Body.jsx
@@ -0,0 +1,23 @@
+import Login from "./Login";
+import Browser from "./Browser";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const Body = () => {
+
+    const appRouter = createBrowserRouter([
+        {
+            path: "/",
+            element: <Login />
+        },
+        {
+            path: "/browse",
+            element: <Browser />
+        }
+    ]);
+
+    return <div>
+        <RouterProvider router={appRouter}/>
+    </div>
+};
+
+export default Body;
