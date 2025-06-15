e9a68ac29a08fb1aa613f8e72328f84af858f990
vaibhav-graphic <vaibhavbhatt59@gmail.com>
2025-06-16 04:36:50 +0530

    add Sign out

--- src/components/Browser.jsx
+++ src/components/Browser.jsx
@@ -1,5 +1,9 @@
+import Header from "./Header";
+
 const Browser = () => {
-    return <div>Browser</div>
+    return <div>
+        <Header />
+    </div>
 };
 
 export default Browser;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,32 @@
+import { signOut } from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+
 import { LOGO_IMG } from "../utils/constants";
+import { USER_ICON_LOGO } from "../utils/constants";
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((error) => {
+        navigate("/error");
+      });
+  };
+
   return (
-    <div className="absolute px-8 py-2 bg-gradient-to-b from-black z-10">
-      <img
-        className="w-44"
-        src={LOGO_IMG}
-        alt="logo"
-      />
+    <div className="absolute w-screen px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+      <img className="w-44" src={LOGO_IMG} alt="logo" />
+      <div className="flex p-2">
+        <img className="w-12 h-12" alt="usericon" src={USER_ICON_LOGO} />
+        <button onClick={handleSignOut} className="font-bold text-white">
+          Sign out
+        </button>
+      </div>
     </div>
   );
 };
